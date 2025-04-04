74d1268dd0b57055fd308c70f3e4e480839afb1d
Cesar Berrospi Ramis <75900930+ceberam@users.noreply.github.com>
2025-04-04 16:45:57 +0200

    build: improve CI/CD pipeline and project settings
    Signed-off-by: Cesar Berrospi Ramis <75900930+ceberam@users.noreply.github.com>

---
+++ tests/test_s3_helper.py
@@ -0,0 +1,8 @@
+from docling_jobkit.connectors.s3_helper import strip_prefix_postfix
+
+def test_strip_prefix_postfix():
+    in_set = {"mypath/json/file_1.json", "mypath/json/file_2.json"}
+    out_set = strip_prefix_postfix(in_set, prefix="mypath/json/", extension=".json")
+
+    assert len(in_set) == len(out_set)
+    assert out_set == {"file_1", "file_2"}
