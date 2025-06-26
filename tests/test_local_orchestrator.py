881cc27394ab4f7e7eb0f0ac6c92a83192c79973
Michele Dolfi <dol@zurich.ibm.com>
2025-06-26 22:40:54 +0200

    move and refactor engines logic from docling-serve
    Signed-off-by: Michele Dolfi <dol@zurich.ibm.com>

---
+++ tests/test_local_orchestrator.py
@@ -0,0 +1,79 @@
+import asyncio
+import time
+
+import pytest
+import pytest_asyncio
+
+from docling.datamodel.base_models import ConversionStatus
+
+from docling_jobkit.convert.manager import (
+    DoclingConverterManager,
+    DoclingConverterManagerConfig,
+)
+from docling_jobkit.datamodel.convert import ConvertDocumentsOptions
+from docling_jobkit.datamodel.http_inputs import HttpSource
+from docling_jobkit.datamodel.task import TaskSource
+from docling_jobkit.orchestrators.local.orchestrator import (
+    LocalOrchestrator,
+    LocalOrchestratorConfig,
+)
+
+
+@pytest_asyncio.fixture
+async def orchestrator():
+    # Setup
+    config = LocalOrchestratorConfig(
+        num_workers=2,
+    )
+
+    cm_config = DoclingConverterManagerConfig()
+    cm = DoclingConverterManager(config=cm_config)
+
+    orchestrator = LocalOrchestrator(config=config, converter_manager=cm)
+    queue_task = asyncio.create_task(orchestrator.process_queue())
+
+    yield orchestrator
+
+    # Teardown
+    # Cancel the background queue processor on shutdown
+    queue_task.cancel()
+    try:
+        await queue_task
+    except asyncio.CancelledError:
+        print("Queue processor cancelled.")
+
+
+async def _wait_task_complete(
+    orchestrator: LocalOrchestrator, task_id: str, max_wait: int = 60
+) -> bool:
+    start_time = time.monotonic()
+    while True:
+        task = await orchestrator.task_status(task_id=task_id)
+        if task.is_completed():
+            return True
+        await asyncio.sleep(5)
+        elapsed_time = time.monotonic() - start_time
+        if elapsed_time > max_wait:
+            return False
+
+
+@pytest.mark.asyncio
+async def test_convert(orchestrator: LocalOrchestrator):
+    options = ConvertDocumentsOptions()
+
+    sources: list[TaskSource] = []
+    sources.append(HttpSource(url="https://arxiv.org/pdf/2311.18481"))
+
+    task = await orchestrator.enqueue(
+        sources=sources,
+        options=options,
+    )
+
+    await _wait_task_complete(orchestrator, task.task_id)
+    results = await orchestrator.task_result(task_id=task.task_id)
+
+    assert results is not None
+    assert len(results) == 1
+
+    result = results[0]
+    assert result.status == ConversionStatus.SUCCESS
---
+++ tests/test_options_serialization.py
@@ -0,0 +1,57 @@
+from docling_jobkit.convert.manager import (
+    DoclingConverterManager,
+    DoclingConverterManagerConfig,
+    _hash_pdf_format_option,
+)
+from docling_jobkit.datamodel.convert import (
+    ConvertDocumentsOptions,
+    PictureDescriptionApi,
+)
+
+
+def test_options_cache_key():
+    hashes = set()
+
+    m = DoclingConverterManager(config=DoclingConverterManagerConfig())
+
+    opts = ConvertDocumentsOptions()
+    pipeline_opts = m.get_pdf_pipeline_opts(opts)
+    hash = _hash_pdf_format_option(pipeline_opts)
+    assert hash not in hashes
+    hashes.add(hash)
+
+    opts.do_picture_description = True
+    pipeline_opts = m.get_pdf_pipeline_opts(opts)
+    hash = _hash_pdf_format_option(pipeline_opts)
+    # pprint(pipeline_opts.pipeline_options.model_dump(serialize_as_any=True))
+    assert hash not in hashes
+    hashes.add(hash)
+
+    opts.picture_description_api = PictureDescriptionApi(
+        url="http://localhost",
+        params={"model": "mymodel"},
+        prompt="Hello 1",
+    )
+    pipeline_opts = m.get_pdf_pipeline_opts(opts)
+    hash = _hash_pdf_format_option(pipeline_opts)
+    # pprint(pipeline_opts.pipeline_options.model_dump(serialize_as_any=True))
+    assert hash not in hashes
+    hashes.add(hash)
+
+    opts.picture_description_api = PictureDescriptionApi(
+        url="http://localhost",
+        params={"model": "your-model"},
+        prompt="Hello 1",
+    )
+    pipeline_opts = m.get_pdf_pipeline_opts(opts)
+    hash = _hash_pdf_format_option(pipeline_opts)
+    # pprint(pipeline_opts.pipeline_options.model_dump(serialize_as_any=True))
+    assert hash not in hashes
+    hashes.add(hash)
+
+    opts.picture_description_api.prompt = "World"
+    pipeline_opts = m.get_pdf_pipeline_opts(opts)
+    hash = _hash_pdf_format_option(pipeline_opts)
+    # pprint(pipeline_opts.pipeline_options.model_dump(serialize_as_any=True))
+    assert hash not in hashes
+    hashes.add(hash)
