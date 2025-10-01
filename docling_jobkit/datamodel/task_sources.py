08cf0768e995652620fe905de9803be7bcf6d7a9
Lucas Morin <73230090+lucas-morin@users.noreply.github.com>
2025-10-01 17:41:56 +0200

    feat: create connectors to import/export documents from/to Google Drive (#62)
    feat: create processors to import/export documents from/to Google Drive
    
    Signed-off-by: Lucas Morin <lucas.morin222@gmail.com>

--- docling_jobkit/datamodel/task_sources.py
+++ docling_jobkit/datamodel/task_sources.py
@@ -2,6 +2,7 @@ from typing import Annotated, Literal
 
 from pydantic import Field
 
+from docling_jobkit.datamodel.google_drive_coords import GoogleDriveCoordinates
 from docling_jobkit.datamodel.http_inputs import FileSource, HttpSource
 from docling_jobkit.datamodel.s3_coords import S3Coordinates
 
@@ -18,6 +19,11 @@ class TaskS3Source(S3Coordinates):
     kind: Literal["s3"] = "s3"
 
 
+class TaskGoogleDriveSource(GoogleDriveCoordinates):
+    kind: Literal["google_drive"] = "google_drive"
+
+
 TaskSource = Annotated[
-    TaskFileSource | TaskHttpSource | TaskS3Source, Field(discriminator="kind")
+    TaskFileSource | TaskHttpSource | TaskS3Source | TaskGoogleDriveSource,
+    Field(discriminator="kind"),
 ]
